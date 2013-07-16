46a6e78038b786e3ab909d0d35e05bb20943e4c8
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-07-16 14:18:59 -0500

    Included the renamed fget.rb (imget.rb) file and added a readme.

---
+++ README.md
@@ -0,0 +1,42 @@
+Imget
+-----
+Imget is a simple solution to downloading and organizing pictures at the same time.
+
+## Table of contents
+[toc]
+
+## Requirements
+- Ruby >= 1.9.3 
+- Trollop >= 2.0
+
+Run bundler to install the gemfile or run `gem install trollop`
+
+** Has not been tested any other Version of Trollop.
+
+## Yaml Format
+
+Imget uses a Yaml file to load in the structure.
+
+The format is as follows:
+
+	Folder/Path/Here/:
+	  - http://website.com/image.jpg
+	Folder/has/to/end/with/a/forwardslash/:
+	  - http://website.com/image.jpg
+	Folder/has/no/beginning/forwardslash/:
+	  - http://website.com/image.jpg
+	  - http://websites.are/indented/with/spaces/not/tabs.gif
+
+The folders should end with a forwardslash and the urls should end in .jpeg(jpg),png, or gif.
+The urls need to be indented using spaces, tabs will throw an error.
+
+Yaml has some [documentation](http://yaml.org/spec/1.2/2009-07-21/spec.html#id2576668) that explains it further
+
+## Instructions
+
+To run imget, run the imget script and use the -i parameter to supply the yaml file.
+
+	ruby imget.rb -i inputlist
+
+In this example, the inputlist did not include the .yml extension, but it will look for inputlist.yml 
+in the current working directory.
---
+++ imget.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+require 'trollop'
+require 'open-uri'
+require 'fileutils'
+require 'yaml'
+
+opts = Trollop::options do 
+  opt :input, "Input list formatted with URLS and folder structure", type: :string, short: "i"
+end
+
+unless opts[:input_given]
+  print "Please supply an input list"
+  exit(1)
+end
+pwd = Dir::pwd + "/"
+
+if opts[:input].match(/\.yml/)
+  inputname = opts[:input]
+else
+  inputname = opts[:input]+".yml"
+end
+
+p inputname
+
+unless File.exists?("#{pwd}#{inputname}")
+  print "#{pwd}#{inputname} does not exist or has insufficient permissions\n"
+  exit(2)
+end
+
+inputlist = YAML.load_file(pwd+inputname)
+
+p inputlist
+
+inputlist.each do |k,v|
+  tgt = pwd+k
+  unless File.exist?(tgt)
+    FileUtils::mkdir_p(tgt)
+  end
+
+  v.each do |link|
+    urlchunks = link.split("/")
+    filename = urlchunks.last
+    unless File.exists?(tgt+filename)
+      begin
+        print "Retrieving #{filename}\n"
+        filename.match(/[a-zA-Z1-9]+\.(?:jpe?g|gif|png)/i)[0]
+        filesrc = open(link).read
+        file = open(tgt+filename,"wb")
+        file.write(filesrc)
+        file.close
+      rescue NoMethodError
+        print "There seems to be an error processing #{filename}. Please Review #{inputname} for errors.\n"
+      rescue OpenURI::HTTPError => e
+        print "There seems to be an error processing #{filename}. #{e.to_s}\n"
+      end
+    else
+      print "#{filename} already exists, skipping\n"
+    end
+  end
+end
