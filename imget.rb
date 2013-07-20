f7566b671e28905a39b55c751442dab2fd5eb314
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-07-19 22:01:22 -0500

    Fixed an error with empty folders.

--- imget.rb
+++ imget.rb
@@ -21,7 +21,7 @@ else
   inputname = opts[:input]+".yml"
 end
 
-p inputname
+#p inputname
 
 unless File.exists?("#{pwd}#{inputname}")
   print "#{pwd}#{inputname} does not exist or has insufficient permissions\n"
@@ -30,7 +30,7 @@ end
 
 inputlist = YAML.load_file(pwd+inputname)
 
-p inputlist
+#p inputlist
 
 inputlist.each do |k,v|
   tgt = pwd+k
@@ -38,24 +38,26 @@ inputlist.each do |k,v|
     FileUtils::mkdir_p(tgt)
   end
 
-  v.each do |link|
-    urlchunks = link.split("/")
-    filename = urlchunks.last
-    unless File.exists?(tgt+filename)
-      begin
-        print "Retrieving #{filename}\n"
-        filename.match(/[a-zA-Z1-9]+\.(?:jpe?g|gif|png)/i)[0]
-        filesrc = open(link).read
-        file = open(tgt+filename,"wb")
-        file.write(filesrc)
-        file.close
-      rescue NoMethodError
-        print "There seems to be an error processing #{filename}. Please Review #{inputname} for errors.\n"
-      rescue OpenURI::HTTPError => e
-        print "There seems to be an error processing #{filename}. #{e.to_s}\n"
+  unless v.nil?
+    v.each do |link|
+      urlchunks = link.split("/")
+      filename = urlchunks.last
+      unless File.exists?(tgt+filename)
+        begin
+          print "Retrieving #{filename}\n"
+          filename.match(/[a-zA-Z1-9]+\.(?:jpe?g|gif|png)/i)[0]
+          filesrc = open(link).read
+          file = open(tgt+filename,"wb")
+          file.write(filesrc)
+          file.close
+        rescue NoMethodError
+          print "There seems to be an error processing #{filename}. Please Review #{inputname} for errors.\n"
+        rescue OpenURI::HTTPError => e
+          print "There seems to be an error processing #{filename}. #{e.to_s}\n"
+        end
+      else
+        print "#{filename} already exists, skipping\n"
       end
-    else
-      print "#{filename} already exists, skipping\n"
     end
   end
 end
